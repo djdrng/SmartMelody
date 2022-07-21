6bbc1450bda99cb3f48e1f1bb1e6d4b957242d38
Ayan Khan <am37khan@uwaterloo.ca>
2022-07-21 14:20:58 -0400

    add hugging face sentiment analysis api

---
+++ parsing_and_analysis/main.py
@@ -0,0 +1,15 @@
+
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+
+emotion = pipeline('sentiment-analysis', 
+                    model='arpanghoshal/EmoRoBERTa')
+
+
+def analyzeText(input):
+    emotion_labels = emotion(input)
+    return emotion_labels
+
+
